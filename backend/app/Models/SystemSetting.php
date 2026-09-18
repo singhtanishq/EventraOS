fd3a0202599a5a8990a5b06f4befd4894c3f5940
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:15:18 +0530

    Update SystemSetting.php

--- backend/app/Models/SystemSetting.php
+++ backend/app/Models/SystemSetting.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SystemSetting extends Model
 {
+    protected $table = 'system_settings';
+
     protected $fillable = ['key', 'group', 'value', 'type', 'description', 'is_public', 'is_encrypted', 'validation_rules', 'options', 'sort_order'];
     protected $casts = ['is_public' => 'boolean', 'is_encrypted' => 'boolean', 'validation_rules' => 'array', 'options' => 'array'];
 }
