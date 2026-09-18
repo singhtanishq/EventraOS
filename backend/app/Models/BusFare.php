0dfdf9b33dbdcd161ad3009f29dcb0f75b6041c8
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:08:51 +0530

    Update BusFare.php

--- backend/app/Models/BusFare.php
+++ backend/app/Models/BusFare.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BusFare extends Model
 {
+    protected $table = 'bus_fares';
+
     protected $fillable = ['bus_route_id', 'bus_type_id', 'provider_id', 'provider_fare_id', 'base_fare', 'tax_amount', 'fee_amount', 'currency', 'cancellation_policy', 'is_active'];
     protected $casts = ['base_fare' => 'decimal:4', 'tax_amount' => 'decimal:4', 'fee_amount' => 'decimal:4', 'cancellation_policy' => 'array', 'is_active' => 'boolean'];
 
