5bfc0799462f6676142c42e61a0bc71906b0a138
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:14:12 +0530

    Update RefundItem.php

--- backend/app/Models/RefundItem.php
+++ backend/app/Models/RefundItem.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RefundItem extends Model
 {
+    protected $table = 'refund_items';
+
     protected $fillable = ['refund_id', 'booking_item_id', 'amount', 'currency'];
 
     protected $casts = ['amount' => 'decimal:4'];
