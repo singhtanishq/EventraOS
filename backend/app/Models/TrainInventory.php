7d34f1a289a672b1c23ff69dc5a9947bdac9d34d
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:15:43 +0530

    Update TrainInventory.php

--- backend/app/Models/TrainInventory.php
+++ backend/app/Models/TrainInventory.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TrainInventory extends Model
 {
+    protected $table = 'train_inventory';
+
     protected $fillable = ['train_route_id', 'class_id', 'fare_id', 'journey_date', 'total_berths', 'available_berths', 'booked_berths', 'rac_count', 'wl_count', 'current_fare', 'is_closed'];
     protected $casts = ['journey_date' => 'date', 'current_fare' => 'decimal:4', 'is_closed' => 'boolean'];
 
