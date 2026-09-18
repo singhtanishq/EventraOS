19fd4789ef4f33f150b0546a054373b4c9a0d69b
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:10:14 +0530

    Update CarRate.php

--- backend/app/Models/CarRate.php
+++ backend/app/Models/CarRate.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CarRate extends Model
 {
+    protected $table = 'car_rates';
+
     protected $fillable = ['car_id', 'provider_id', 'name', 'rate_type', 'base_rate', 'km_included', 'extra_km_rate', 'driver_allowance', 'insurance_options', 'deposit_amount', 'currency', 'cancellation_policy', 'terms_conditions', 'is_active'];
     protected $casts = ['base_rate' => 'decimal:4', 'extra_km_rate' => 'decimal:4', 'driver_allowance' => 'decimal:4', 'deposit_amount' => 'decimal:4', 'insurance_options' => 'array', 'cancellation_policy' => 'array', 'terms_conditions' => 'array', 'is_active' => 'boolean'];
 
