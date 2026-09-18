41a85aaeef73f50b2ac38a389318952c4b9f1888
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:12:41 +0530

    Update NotificationPreference.php

--- backend/app/Models/NotificationPreference.php
+++ backend/app/Models/NotificationPreference.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class NotificationPreference extends Model
 {
+    protected $table = 'notification_preferences';
+
     protected $fillable = ['notifiable_type', 'notifiable_id', 'channels', 'types', 'quiet_hours'];
     protected $casts = ['channels' => 'array', 'types' => 'array', 'quiet_hours' => 'array'];
 
