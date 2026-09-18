2b92072bde8a58f7500e4486ca2e0f40d5b3cd36
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:14:45 +0530

    Update SearchHistory.php

--- backend/app/Models/SearchHistory.php
+++ backend/app/Models/SearchHistory.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SearchHistory extends Model
 {
+    protected $table = 'search_history';
+
     protected $fillable = ['customer_id', 'session_id', 'search_type', 'search_parameters', 'results_count', 'selected_result_id', 'selected_result_type'];
     protected $casts = ['search_parameters' => 'array'];
 }
