1a5a85645f173cfdbac753b987fc223b3deb5d6a
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:14:20 +0530

    Update Reschedule.php

--- backend/app/Models/Reschedule.php
+++ backend/app/Models/Reschedule.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Reschedule extends Model
 {
+    protected $table = 'reschedules';
+
     protected $fillable = [
         'uuid', 'reschedule_reference', 'booking_id', 'booking_item_id',
         'customer_id', 'requested_by', 'status', 'original_schedule',
