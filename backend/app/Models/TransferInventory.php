8fc52e682c066ce959e75f06d37c39697836dc86
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:15:52 +0530

    Update TransferInventory.php

--- backend/app/Models/TransferInventory.php
+++ backend/app/Models/TransferInventory.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TransferInventory extends Model
 {
+    protected $table = 'transfer_inventory';
+
     protected $fillable = ['transfer_id', 'pricing_id', 'date', 'time_slot', 'total_vehicles', 'available_vehicles', 'booked_vehicles', 'price_override', 'is_closed'];
     protected $casts = ['date' => 'date', 'price_override' => 'decimal:4', 'is_closed' => 'boolean'];
 
