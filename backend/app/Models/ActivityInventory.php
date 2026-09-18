6b8fbe783853996b47e0960d4dc030883065cd28
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:06:58 +0530

    Update ActivityInventory.php

--- backend/app/Models/ActivityInventory.php
+++ backend/app/Models/ActivityInventory.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ActivityInventory extends Model
 {
+    protected $table = 'activity_inventory';
+
     protected $fillable = ['activity_id', 'schedule_id', 'pricing_id', 'date', 'total_slots', 'available_slots', 'booked_slots', 'price_override', 'is_closed'];
     protected $casts = ['date' => 'date', 'price_override' => 'decimal:4', 'is_closed' => 'boolean'];
 
