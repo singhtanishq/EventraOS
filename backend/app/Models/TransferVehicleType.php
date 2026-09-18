34b95054e4db13429a8007cc768ce43895944734
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:16:17 +0530

    Update TransferVehicleType.php

--- backend/app/Models/TransferVehicleType.php
+++ backend/app/Models/TransferVehicleType.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class TransferVehicleType extends Model
 {
+    protected $table = 'transfer_vehicle_types';
+
     protected $fillable = ['name', 'code', 'max_passengers', 'max_luggage', 'features', 'has_driver', 'sort_order', 'is_active'];
     protected $casts = ['features' => 'array', 'has_driver' => 'boolean', 'is_active' => 'boolean'];
 }
