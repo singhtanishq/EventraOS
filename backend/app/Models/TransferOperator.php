fbd48d7c58c198d8f3f0083d3cfdc394f3f242e4
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:16:00 +0530

    Update TransferOperator.php

--- backend/app/Models/TransferOperator.php
+++ backend/app/Models/TransferOperator.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class TransferOperator extends Model
 {
+    protected $table = 'transfer_operators';
+
     protected $fillable = ['uuid', 'provider_id', 'name', 'code', 'logo', 'contact_phone', 'contact_email', 'is_active', 'is_demo', 'metadata'];
     protected $casts = ['is_active' => 'boolean', 'is_demo' => 'boolean', 'metadata' => 'array'];
 
