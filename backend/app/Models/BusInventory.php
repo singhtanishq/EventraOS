b256600eaf178f8aea7b43d01e0f766285b116d6
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:08:59 +0530

    Update BusInventory.php

--- backend/app/Models/BusInventory.php
+++ backend/app/Models/BusInventory.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BusInventory extends Model
 {
+    protected $table = 'bus_inventory';
+
     protected $fillable = ['bus_route_id', 'bus_type_id', 'fare_id', 'journey_date', 'total_seats', 'available_seats', 'booked_seats', 'blocked_seats', 'current_fare', 'seat_status', 'is_cancelled'];
     protected $casts = ['journey_date' => 'date', 'current_fare' => 'decimal:4', 'seat_status' => 'array', 'is_cancelled' => 'boolean'];
 
