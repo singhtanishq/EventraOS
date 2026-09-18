734229684e46b0eb0584bec573a5d5201fda8bce
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:14:28 +0530

    Update ReviewVote.php

--- backend/app/Models/ReviewVote.php
+++ backend/app/Models/ReviewVote.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ReviewVote extends Model
 {
+    protected $table = 'review_votes';
+
     protected $fillable = ['review_id', 'customer_id', 'vote'];
 
     public function review(): BelongsTo { return $this->belongsTo(Review::class); }
