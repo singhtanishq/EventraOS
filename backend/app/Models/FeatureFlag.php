d8f92e92a54ba12f61d674048857d6027968bbde
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-19 04:11:18 +0530

    Update FeatureFlag.php

--- backend/app/Models/FeatureFlag.php
+++ backend/app/Models/FeatureFlag.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class FeatureFlag extends Model
 {
+    protected $table = 'feature_flags';
+
     protected $fillable = ['key', 'name', 'description', 'enabled', 'conditions', 'variants', 'is_permanent'];
     protected $casts = ['enabled' => 'boolean', 'conditions' => 'array', 'variants' => 'array', 'is_permanent' => 'boolean'];
 }
