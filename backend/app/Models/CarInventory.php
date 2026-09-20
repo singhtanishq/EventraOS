5f6671b3cd01e3f8d1168bd5eb624e01a6d2de84
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-20 14:59:46 +0530

    Update CarInventory.php

--- backend/app/Models/CarInventory.php
+++ backend/app/Models/CarInventory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CarInventory extends Model
 {
-    protected $table = 'car_inventories';
+    protected $table = 'car_inventory';
 
     protected $fillable = ['car_id', 'rate_id', 'date', 'status', 'booking_id', 'price_override'];
     protected $casts = ['date' => 'date', 'price_override' => 'decimal:4'];
