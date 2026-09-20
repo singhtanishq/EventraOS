11a8304749bd8ef5ed3587ac06652eab2df54a5b
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-20 21:06:30 +0530

    Update Dropdown.tsx

--- frontend/src/components/ui/Dropdown.tsx
+++ frontend/src/components/ui/Dropdown.tsx
@@ -202,7 +202,6 @@ export const Dropdown = forwardRef<HTMLDivElement, DropdownProps>(
                     )}
                   </button>
                 ))}
-              )}
             </div>
           </motion.div>
         )}
