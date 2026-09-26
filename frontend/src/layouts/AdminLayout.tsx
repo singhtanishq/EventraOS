40d2bcce61a5c7eea34de61798ca051f46d3360a
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-26 14:01:37 +0530

    Update AdminLayout.tsx

--- frontend/src/layouts/AdminLayout.tsx
+++ frontend/src/layouts/AdminLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, NavLink, useLocation } from 'react-router-dom'
+import { Outlet, NavLink, Link, useLocation } from 'react-router-dom'
 import { useAuthStore } from '@/store/auth'
 import { cn } from '@/lib/utils'
 import {
