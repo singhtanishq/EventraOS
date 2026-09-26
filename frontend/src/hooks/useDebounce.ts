4fba3a5b6022b7499be9d44257e3d782aa840361
Tanishq Singh <tanishqsingh@outlook.in>
2026-09-26 14:12:16 +0530

    Update useDebounce.ts

--- frontend/src/hooks/useDebounce.ts
+++ frontend/src/hooks/useDebounce.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 export function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState(value)
